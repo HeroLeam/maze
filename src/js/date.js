d59422ec5c27ee7f1422752dbf2d5424d70b6e6e
HeroLeam <thiagoleam@gmail.com>
2024-05-01 16:41:00 -0300

    Ajustes
    Novo seleção (entrada)
    Layout novo
    Adicionado Hora

--- src/js/date.js
+++ src/js/date.js
@@ -2,6 +2,8 @@ var dataAtual = new Date();
 var dia = String(dataAtual.getDate()).padStart(2, '0');
 var mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
 var ano = dataAtual.getFullYear();
-var dataFormatada = dia + '/' + mes + '/' + ano;
+var hora = String(dataAtual.getHours()).padStart(2, '0');
+var minuto = String(dataAtual.getMinutes()).padStart(2, '0');
+var dataFormatada = dia + '/' + mes + '/' + ano + ' - ' + hora + ':' + minuto;
 
-document.getElementById('data-atual').textContent = dataFormatada;
+document.getElementById('daily').textContent = dataFormatada;
--- src/js/select.js
+++ src/js/select.js
@@ -21,6 +21,7 @@ function trocarImagem(selectId, imgSrc, index) {
 }
 
 function resetarSelecao() {
+  document.getElementById('entrada').value = 'nenhum';
   document.getElementById('saida-certa').value = 'nenhum';
   document.getElementById('saida-errada').value = 'nenhum';
   document.getElementById('guard-esquerda').value = 'nenhum';
@@ -46,60 +47,64 @@ function resetarSelecao() {
   });
 }
 
-document.getElementById('saida-certa').addEventListener('change', function() {
-  trocarImagem('saida-certa', this.value, 1);
+document.getElementById('entrada').addEventListener('change', function() {
+  trocarImagem('entrada', this.value, 1);
 });
 
 document.getElementById('saida-errada').addEventListener('change', function() {
-  trocarImagem('saida-errada', this.value, 2);
+  trocarImagem('saida-certa', this.value, 2);
+});
+
+document.getElementById('saida-errada').addEventListener('change', function() {
+  trocarImagem('saida-errada', this.value, 3);
 });
 
 document.getElementById('guard-esquerda').addEventListener('change', function() {
-  trocarImagem('guard-esquerda', this.value, 3);
+  trocarImagem('guard-esquerda', this.value, 4);
 });
 
 document.getElementById('guard-direita').addEventListener('change', function() {
-  trocarImagem('guard-direita', this.value, 4);
+  trocarImagem('guard-direita', this.value, 5);
 });
 
 document.getElementById('biblioteca-esquerda').addEventListener('change', function() {
-  trocarImagem('biblioteca-esquerda', this.value, 5);
+  trocarImagem('biblioteca-esquerda', this.value, 6);
 });
 
 document.getElementById('biblioteca-direta').addEventListener('change', function() {
-  trocarImagem('biblioteca-direta', this.value, 6);
+  trocarImagem('biblioteca-direta', this.value, 7);
 });
 
 document.getElementById('ponte-fina-esquerda').addEventListener('change', function() {
-  trocarImagem('ponte-fina-esquerda', this.value, 7);
+  trocarImagem('ponte-fina-esquerda', this.value, 8);
 });
 
 document.getElementById('ponte-fina-direita').addEventListener('change', function() {
-  trocarImagem('ponte-fina-direita', this.value, 8);
+  trocarImagem('ponte-fina-direita', this.value, 9);
 });
 
 document.getElementById('ilha-l-cima').addEventListener('change', function() {
-  trocarImagem('ilha-l-cima', this.value, 9);
+  trocarImagem('ilha-l-cima', this.value, 10);
 });
 
 document.getElementById('ilha-l-baixo').addEventListener('change', function() {
-  trocarImagem('ilha-l-baixo', this.value, 10);
+  trocarImagem('ilha-l-baixo', this.value, 11);
 });
 
 document.getElementById('coluna-cima').addEventListener('change', function() {
-  trocarImagem('coluna-cima', this.value, 11);
+  trocarImagem('coluna-cima', this.value, 12);
 });
 
 document.getElementById('coluna-baixo').addEventListener('change', function() {
-  trocarImagem('coluna-baixo', this.value, 12);
+  trocarImagem('coluna-baixo', this.value, 13);
 });
 
 document.getElementById('ponte-grossa-esquerda').addEventListener('change', function() {
-  trocarImagem('ponte-grossa-esquerda', this.value, 13);
+  trocarImagem('ponte-grossa-esquerda', this.value, 14);
 });
 
 document.getElementById('ponte-grossa-direita').addEventListener('change', function() {
-  trocarImagem('ponte-grossa-direita', this.value, 14);
+  trocarImagem('ponte-grossa-direita', this.value, 15);
 });
 
 document.getElementById('reset-btn').addEventListener('click', function() {
